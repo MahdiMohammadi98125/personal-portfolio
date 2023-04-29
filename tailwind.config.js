656f037c2a76f170f6acdb5ea4770cc5e7ea1736
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2023-04-29 17:31:57 +0430

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-1": "#111",
+        "dark-2": "#222",
+        "dark-3": "#333",
+        "dark-4": "#444",
+        "dark-5": "#AAA",
+      },
+    },
   },
   plugins: [],
 };
